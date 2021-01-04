73be65dff2ef21eec8275b396f88bff345add4ff
Merwa95 <merwamanssar@gmail.com>
2021-01-04 02:05:09 +0100

    Creation Layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeLayoutComponent } from './layouts/employe-layout/employe-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeLayoutComponent,
+    AdminLayoutComponent,
+    ClientLayoutComponent,
+    LoginLayoutComponent
   ],
   imports: [
     BrowserModule,
